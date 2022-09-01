f3c96aac0a832d4cb2e5ec6e5d6a6c5e89bf03b3
Mohammad Mujib <mohammadmujib09@gmail.com>
2022-09-01 18:18:25 +0000

    folder

---
+++ c++/hello.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    /* std::vector<int> v = {1, 2, 3, 4, 5}; */
+    int t;
+    cin>>t;
+    cout<<t;
+    // vector<int> v(10, 5);
+    // int sum = 0;
+    // for (int i = 0; i < v.size(); i++){
+    //     sum += v[i]*2;
+    // }
+    // cout << "sum is " << sum << endl;
+    return 0;
+}                                                      
